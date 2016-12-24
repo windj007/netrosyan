0fa46bc461901b5fbd646926f8dc71a8f26fed1a
Roman <windj007@gmail.com>
2016-12-24 09:12:05 +0300

    first commit: import data, build a simple character-rnn

---
+++ run_container.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+YOUR_HASHED_PASSWORD=$(cat jupyter.passwd)
+
+BIND_PORT="-p 8889:8888"
+if (( $# >= 1 ))
+then
+    BIND_PORT=""
+fi
+
+
+CMD="docker"
+if which nvidia-docker
+then
+    CMD="nvidia-docker"
+fi
+
+$CMD run -ti --rm \
+    -e "HASHED_PASSWORD=$YOUR_HASHED_PASSWORD" \
+    -e "SSL=" \
+    -v /home/rsuvorov/projects/docker-jupyter-keras-tools/certs:/jupyter/certs \
+    -v `pwd`:/notebook \
+    $BIND_PORT \
+    windj007/jupyter-keras-tools \
+    $@
